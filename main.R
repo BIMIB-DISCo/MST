4f52a5f995fde0088a9da7c22f09504d43d31139
luca-dex <luca.desano@gmail.com>
2016-04-06 19:02:44 +0200

    boooom: parallel!

--- main.R
+++ main.R
@@ -11,29 +11,29 @@
 #                                                                                #
 ##################################################################################
 
-# Set SINK to FALSE to avoid creating a log file
-SINK = TRUE
-if(SINK) sink(paste0(getwd(), "/PiCnIc-COADREAD-logfile.txt"), append=FALSE, split=TRUE)
-
 # source the needed script
-library("TRONCO")
+library(TRONCO)
+library(parallel)
+dir.create('RData')
 
 
-source('./sample.polyclonal.trees.R')
-source('./perform.polyclonal.tree.experiments.R')
-source('./statistics.R')
-
-# set the seed
-set.seed(12345)
+source('sample.polyclonal.trees.R')
+source('perform.polyclonal.tree.experiments.R')
+source('statistics.R')
 
 # setting of the experiments
-number_experiments = 100
+seed = 12345
+number_experiments = 3
 my_experiments = 1:number_experiments
 names(my_experiments) = paste("Experiment",my_experiments)
 sample_levels = 10
 noise_levels = 8
 my_algorithms = c("capri","caprese","edmonds","chowliu","prim")
 my_regularizators = c("no.reg.res","loglik.res","aic.res","bic.res")
+cores.ratio = 1
+
+# set the seed
+set.seed(seed)
 
 # set the true trees
 low_true_tree = array(0,c(6,6))
@@ -74,7 +74,7 @@ high_true_tree[11,16] = 1
 high_true_tree[11,17] = 1
 
 # setting for single cell
-sampele_sizes_single_cells = c(10, 25, 50, 75, 100, 150, 200, 250, 500, 1000)
+sample_sizes_single_cells = c(10, 25, 50, 75, 100, 150, 200, 250, 500, 1000)
 e_pos_single_cells = c(0.000, 0.005, 0.010, 0.015, 0.020, 0.025, 0.030, 0.035)
 e_neg_single_cells = c(0.000, 0.050, 0.100, 0.150, 0.200, 0.250, 0.300, 0.350)
 low_probs_single_cells = c(0.9,0.6,0.6,0.7,0.8,0.7)
@@ -86,57 +86,194 @@ clones_num_sampling_low = 3
 clones_num_sampling_medium = 5
 clones_num_sampling_high = 8
 wild_type_rate = 0.1
-sampele_sizes_multiple_biopses = c(5, 6, 7, 8, 9, 10, 15, 20, 50, 100)
+sample_sizes_multiple_biopses = c(5, 6, 7, 8, 9, 10, 15, 20, 50, 100)
 e_pos_multiple_biopses = c(0.000, 0.005, 0.010, 0.015, 0.020, 0.025, 0.030, 0.035)
 e_neg_multiple_biopses = c(0.000, 0.050, 0.100, 0.150, 0.200, 0.250, 0.300, 0.350)
 low_probs_multiple_biopses = c(1.0,0.6,0.6,0.7,0.8,0.7)
 medium_probs_multiple_biopses = c(1.0,0.6,0.6,0.7,0.8,0.7,0.9,0.8,0.7,0.7,0.6)
 high_probs_multiple_biopses = c(1.0,0.6,0.6,0.7,0.8,0.7,0.9,0.8,0.7,0.7,0.6,0.8,0.7,0.6,0.7,0.7,0.8)
 
+
+cores = as.integer(cores.ratio * (detectCores() - 1))
+if (cores < 1) {
+    cores = 1
+}
+
+cl = makeCluster(cores)
+clusterEvalQ(cl, source('sample.polyclonal.trees.R'))
+clusterEvalQ(cl, source('perform.polyclonal.tree.experiments.R'))
+clusterEvalQ(cl, source('statistics.R'))
+clusterEvalQ(cl, library(TRONCO))
+clusterExport(cl, c('sample_sizes_single_cells',
+    'low_probs_single_cells',
+    'medium_probs_single_cells',
+    'high_probs_single_cells',
+    'e_pos_single_cells',
+    'e_neg_single_cells'))
+clusterExport(cl, c('low_true_tree',
+    'medium_true_tree',
+    'high_true_tree'))
+clusterExport(cl, c('clones_num_sampling_low',
+    'clones_num_sampling_medium',
+    'clones_num_sampling_high',
+    'wild_type_rate',
+    'sample_sizes_multiple_biopses',
+    'e_pos_multiple_biopses',
+    'e_neg_multiple_biopses',
+    'low_probs_multiple_biopses',
+    'medium_probs_multiple_biopses',
+    'high_probs_multiple_biopses'))
+
+
+cat('Using', cores, 'cores via "parallel" \n')
+
 # run the experiments for single cells low
-experiments.single.cells.low = sapply(my_experiments,function(x){run.experiments.single.cells ("low",low_true_tree,sampele_sizes_single_cells,low_probs_single_cells,e_pos_single_cells,e_neg_single_cells)})
-save(experiments.single.cells.low,file="RData/experiments.single.cells.low.RData")
+cat('experiments.single.cells.low\n')
+experiments.single.cells.low = parSapply(cl, my_experiments, function(x){
+    run.experiments.single.cells("low",
+        low_true_tree,
+        sample_sizes_single_cells,
+        low_probs_single_cells,
+        e_pos_single_cells,
+        e_neg_single_cells)
+})
+
+save(experiments.single.cells.low, file="RData/experiments.single.cells.low.RData")
 
 # statistics for the experiments for single cells low
-experiments.single.cells.low.stats = get.stats(experiments.single.cells.low,my_algorithms,my_regularizators,sample_levels,noise_levels,number_experiments)
-save(experiments.single.cells.low.stats,file="RData/experiments.single.cells.low.stats.RData")
+experiments.single.cells.low.stats = get.stats(experiments.single.cells.low,
+    my_algorithms,
+    my_regularizators,
+    sample_levels,
+    noise_levels,
+    number_experiments)
+save(experiments.single.cells.low.stats, file="RData/experiments.single.cells.low.stats.RData")
+
+
+
 
 # run the experiments for single cells medium
-experiments.single.cells.medium = sapply(my_experiments,function(x){run.experiments.single.cells ("medium",medium_true_tree,sampele_sizes_single_cells,medium_probs_single_cells,e_pos_single_cells,e_neg_single_cells)})
-save(experiments.single.cells.medium,file="RData/experiments.single.cells.medium.RData")
+cat('experiments.single.cells.medium\n')
+experiments.single.cells.medium = parSapply(cl, my_experiments, function(x){
+    run.experiments.single.cells("medium",
+        medium_true_tree,
+        sample_sizes_single_cells,
+        medium_probs_single_cells,
+        e_pos_single_cells,
+        e_neg_single_cells)
+})
+save(experiments.single.cells.medium, file="RData/experiments.single.cells.medium.RData")
 
 # statistics for the experiments for single cells medium
-experiments.single.cells.medium.stats = get.stats(experiments.single.cells.medium,my_algorithms,my_regularizators,sample_levels,noise_levels,number_experiments)
-save(experiments.single.cells.medium.stats,file="RData/experiments.single.cells.medium.stats.RData")
+experiments.single.cells.medium.stats = get.stats(experiments.single.cells.medium,
+    my_algorithms,
+    my_regularizators,
+    sample_levels,
+    noise_levels,
+    number_experiments)
+save(experiments.single.cells.medium.stats, file="RData/experiments.single.cells.medium.stats.RData")
+
+
+
 
 # run the experiments for single cells high
-experiments.single.cells.high = sapply(my_experiments,function(x){run.experiments.single.cells ("high",high_true_tree,sampele_sizes_single_cells,high_probs_single_cells,e_pos_single_cells,e_neg_single_cells)})
-save(experiments.single.cells.high,file="RData/experiments.single.cells.high.RData")
+cat('experiments.single.cells.high\n')
+experiments.single.cells.high = parSapply(cl, my_experiments, function(x){
+    run.experiments.single.cells("high",
+        high_true_tree,
+        sample_sizes_single_cells,
+        high_probs_single_cells,
+        e_pos_single_cells,
+        e_neg_single_cells)
+})
+save(experiments.single.cells.high, file="RData/experiments.single.cells.high.RData")
 
 # statistics for the experiments for single cells high
-experiments.single.cells.high.stats = get.stats(experiments.single.cells.high,my_algorithms,my_regularizators,sample_levels,noise_levels,number_experiments)
+experiments.single.cells.high.stats = get.stats(experiments.single.cells.high,
+    my_algorithms,
+    my_regularizators,
+    sample_levels,
+    noise_levels,
+    number_experiments)
 save(experiments.single.cells.high.stats,file="RData/experiments.single.cells.high.stats.RData")
 
+
+
+
 # run the experiments for multiple biopses low
-experiments.multiple.biopses.low = sapply(my_experiments,function(x){run.experiments.multiple.biopses ("low",low_true_tree,sampele_sizes_multiple_biopses,clones_num_sampling_low, low_probs_multiple_biopses,e_pos_multiple_biopses,e_neg_multiple_biopses,wild_type_rate)})
+cat('experiments.multiple.biopses.low\n')
+experiments.multiple.biopses.low = parSapply(cl, my_experiments, function(x){
+    run.experiments.multiple.biopses("low",
+        low_true_tree,
+        sample_sizes_multiple_biopses,
+        clones_num_sampling_low,
+        low_probs_multiple_biopses,
+        e_pos_multiple_biopses,
+        e_neg_multiple_biopses,
+        wild_type_rate)
+})
 save(experiments.multiple.biopses.low,file="RData/experiments.multiple.biopses.low.RData")
 
 # statistics for the experiments for multiple biopses low
-experiments.multiple.biopses.low.stats = get.stats(experiments.multiple.biopses.low,my_algorithms,my_regularizators,sample_levels,noise_levels,number_experiments)
-save(experiments.multiple.biopses.low.stats,file="RData/experiments.multiple.biopses.low.stats.RData")
+experiments.multiple.biopses.low.stats = get.stats(experiments.multiple.biopses.low,
+    my_algorithms,
+    my_regularizators,
+    sample_levels,
+    noise_levels,
+    number_experiments)
+save(experiments.multiple.biopses.low.stats, file="RData/experiments.multiple.biopses.low.stats.RData")
+
+
+
 
 # run the experiments for multiple biopses medium
-experiments.multiple.biopses.medium = sapply(my_experiments,function(x){run.experiments.multiple.biopses ("medium",medium_true_tree,sampele_sizes_multiple_biopses,clones_num_sampling_medium,medium_probs_multiple_biopses,e_pos_multiple_biopses,e_neg_multiple_biopses,wild_type_rate)})
+cat('experiments.multiple.biopses.medium\n')
+experiments.multiple.biopses.medium = parSapply(cl, my_experiments, function(x){
+    run.experiments.multiple.biopses("medium",
+        medium_true_tree,
+        sample_sizes_multiple_biopses,
+        clones_num_sampling_medium,
+        medium_probs_multiple_biopses,
+        e_pos_multiple_biopses,
+        e_neg_multiple_biopses,
+        wild_type_rate)
+})
 save(experiments.multiple.biopses.medium,file="RData/experiments.multiple.biopses.medium.RData")
 
 # statistics for the experiments for multiple biopses medium
-experiments.multiple.biopses.medium.stats = get.stats(experiments.multiple.biopses.medium,my_algorithms,my_regularizators,sample_levels,noise_levels,number_experiments)
+experiments.multiple.biopses.medium.stats = get.stats(experiments.multiple.biopses.medium,
+    my_algorithms,
+    my_regularizators,
+    sample_levels,
+    noise_levels,
+    number_experiments)
 save(experiments.multiple.biopses.medium.stats,file="RData/experiments.multiple.biopses.medium.stats.RData")
 
+
+
+
 # run the experiments for multiple biopses high
-experiments.multiple.biopses.high = sapply(my_experiments,function(x){run.experiments.multiple.biopses ("high",high_true_tree,sampele_sizes_multiple_biopses,clones_num_sampling_high, high_probs_multiple_biopses,e_pos_multiple_biopses,e_neg_multiple_biopses,wild_type_rate)})
+cat('experiments.multiple.biopses.high\n')
+experiments.multiple.biopses.high = parSapply(cl, my_experiments, function(x){
+    run.experiments.multiple.biopses("high",
+        high_true_tree,
+        sample_sizes_multiple_biopses,
+        clones_num_sampling_high,
+        high_probs_multiple_biopses,
+        e_pos_multiple_biopses,
+        e_neg_multiple_biopses,
+        wild_type_rate)
+})
 save(experiments.multiple.biopses.high,file="RData/experiments.multiple.biopses.high.RData")
 
 # statistics for experiments for multiple biopses high
-experiments.multiple.biopses.high.stats = get.stats(experiments.multiple.biopses.high,my_algorithms,my_regularizators,sample_levels,noise_levels,number_experiments)
+experiments.multiple.biopses.high.stats = get.stats(experiments.multiple.biopses.high,
+    my_algorithms,
+    my_regularizators,
+    sample_levels,
+    noise_levels,
+    number_experiments)
 save(experiments.multiple.biopses.high.stats,file="RData/experiments.multiple.biopses.high.stats.RData")
+
+#stop the cluster
+stopCluster(cl)
--- statistics.R
+++ statistics.R
@@ -1,5 +1,20 @@
+##################################################################################
+#                                                                                #
+# MST                                                                            #
+#                                                                                #
+##################################################################################
+# Copyright (c) 2015, Giulio Caravagna, Luca De Sano, Daniele Ramazzotti         #
+# email: tronco@disco.unimib.it                                                  #
+# All rights reserved. This program and the accompanying materials               #
+# are made available under the terms of the GNU GPL v3.0                         #
+# which accompanies this distribution                                            #
+#                                                                                #
+##################################################################################
+
+
 # function to compute the results at each step
-"getStats" = function(true_matrix,inferred_matrix) {
+getStats <- function(true_matrix,
+	inferred_matrix){
 	
 	# compute the statistics
 	tp = 0
@@ -8,17 +23,14 @@
 	fn = 0
 	for (i in 1:nrow(inferred_matrix)) {
 		for (j in i:ncol(inferred_matrix)) {
-			if(i!=j) {
-				if(true_matrix[i,j]==0 && inferred_matrix[i,j]==0) {
+			if (i != j) {
+				if (true_matrix[i,j] == 0 && inferred_matrix[i,j] == 0) {
 					tn = tn + 1
-				}
-				else if(true_matrix[i,j]==0 && inferred_matrix[i,j]==1) {
+				} else if (true_matrix[i,j] == 0 && inferred_matrix[i,j] == 1) {
 					fp = fp + 1
-				}
-				else if(true_matrix[i,j]==1 && inferred_matrix[i,j]==0) {
+				} else if (true_matrix[i,j] == 1 && inferred_matrix[i,j] == 0) {
 					fn = fn + 1
-				}
-				else if(true_matrix[i,j]==1 && inferred_matrix[i,j]==1) {
+				} else if (true_matrix[i,j] == 1 && inferred_matrix[i,j] == 1) {
 					tp = tp + 1
 				}
 			}
@@ -36,8 +48,14 @@
 	
 }
 
+
 # get the mean of the statistics
-get.stats = function( results, my_algorithms, my_regularizators, sample_levels, noise_levels, number_experiments ) {
+get.stats <- function(results,
+	my_algorithms,
+	my_regularizators,
+	sample_levels,
+	noise_levels,
+	number_experiments) {
 	
 	# structure to save the results
 	my_results = NULL
@@ -46,7 +64,7 @@ get.stats = function( results, my_algorithms, my_regularizators, sample_levels, 
 	for (i in 1:number_experiments) {
 		for (a in my_algorithms) {
 			for (b in my_regularizators) {
-				if(!is.null(results[1,i][[1]][[as.character(1)]][["reconstructions"]][[a]][[b]])) {
+				if (!is.null(results[1,i][[1]][[as.character(1)]][["reconstructions"]][[a]][[b]])) {
 					my.col.names = paste("Sample Level",1:sample_levels)
 					my.row.names = paste("Noise Level",1: noise_levels)
 					curr_result_accuracy = array(0,c(noise_levels,sample_levels))
@@ -66,19 +84,18 @@ get.stats = function( results, my_algorithms, my_regularizators, sample_levels, 
 							curr_result_specificity[l,j] = curr_computed[["specificity"]]
 						}
 					}
-					if(i==1) {
+					if(i == 1) {
 						my_results[["accuracy"]][[a]][[gsub(".res","",b)]] = curr_result_accuracy
 						my_results[["sensitivity"]][[a]][[gsub(".res","",b)]] = curr_result_sensitivity
 						my_results[["specificity"]][[a]][[gsub(".res","",b)]] = curr_result_specificity
-					}
-					else {
+					} else {
 						my_results[["accuracy"]][[a]][[gsub(".res","",b)]] = my_results[["accuracy"]][[a]][[gsub(".res","",b)]] + curr_result_accuracy
 						my_results[["sensitivity"]][[a]][[gsub(".res","",b)]] = my_results[["sensitivity"]][[a]][[gsub(".res","",b)]] + curr_result_sensitivity
 						my_results[["specificity"]][[a]][[gsub(".res","",b)]] = my_results[["specificity"]][[a]][[gsub(".res","",b)]] + curr_result_specificity
 					}
 				}
 			}
-			if(a=="caprese") {
+			if (a == "caprese") {
 				my.col.names = paste("Sample Level",1:sample_levels)
 				my.row.names = paste("Noise Level",1: noise_levels)
 				curr_result_accuracy = array(0,c(noise_levels,sample_levels))
@@ -98,12 +115,11 @@ get.stats = function( results, my_algorithms, my_regularizators, sample_levels, 
 						curr_result_specificity[l,j] = curr_computed[["specificity"]]
 					}
 				}
-				if(i==1) {
+				if (i == 1) {
 					my_results[["accuracy"]][[a]][["no.reg"]] = curr_result_accuracy
 					my_results[["sensitivity"]][[a]][["no.reg"]] = curr_result_sensitivity
 					my_results[["specificity"]][[a]][["no.reg"]] = curr_result_specificity
-				}
-				else {
+				} else {
 					my_results[["accuracy"]][[a]][["no.reg"]] = my_results[["accuracy"]][[a]][["no.reg"]] + curr_result_accuracy
 					my_results[["sensitivity"]][[a]][["no.reg"]] = my_results[["sensitivity"]][[a]][["no.reg"]] + curr_result_sensitivity
 					my_results[["specificity"]][[a]][["no.reg"]] = my_results[["specificity"]][[a]][["no.reg"]] + curr_result_specificity
@@ -126,7 +142,5 @@ get.stats = function( results, my_algorithms, my_regularizators, sample_levels, 
 			}
 		}
 	}
-	
 	return(my_results)
-	
 }
