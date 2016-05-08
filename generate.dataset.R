8453b0755823c5111e5dc717950e513116dd37e9
danro9685 <daniele.ramazzotti@yahoo.com>
2016-05-08 09:33:43 -0700

    generating random single cell trees

--- generate.dataset.R
+++ generate.dataset.R
@@ -113,6 +113,109 @@ generate.dataset.multiple.biopses <- function(type,
     return(results)
 }
 
+# generate a random tree and the probabilities of the associated single cell samples
+generate.random.single.cell.dataset <- function ( nodes, significance = 0.10 ) {
+	
+	# generate a random tree
+	my_tree = generate.random.tree(nodes = nodes, significance = significance)
+	
+	# make an igraph object from the tree
+	curr.igraph = graph.adjacency(my_tree$structure, mode = "directed")
+	all_paths = get.all.shortest.paths(curr.igraph,my_tree$root,my_tree$leaves)$res
+	my_paths = NULL
+	for (i in 1:length(all_paths)) {
+		my_paths[[i]] = as.vector(all_paths[[i]])
+	}
+	
+	# build the dataset
+	random_dataset = c(rep(0, nodes),(1-my_tree$root_prob))
+	for (i in 1:length(my_paths)) {
+		curr_path = my_paths[[i]]
+		for (j in 1:length(curr_path)) {
+			curr_valid_sample = NULL
+			curr_sample_probability = 1
+			for (k in 1:j) {
+				curr_valid_sample = c(curr_valid_sample,curr_path[k])
+				if(k==1) {
+					curr_sample_probability = my_tree$root_prob
+				}
+				else {
+					curr_sample_probability = curr_sample_probability * my_tree$probabilities[curr_path[k-1],curr_path[k]]
+				}
+			}
+			new_valid_sample = rep(0,nodes)
+			for(l in 1:length(curr_valid_sample)) {
+				new_valid_sample[curr_valid_sample[l]] = 1
+			}
+			random_dataset = rbind(random_dataset,c(new_valid_sample,curr_sample_probability))
+		}
+	}
+	random_dataset = unique(random_dataset)
+	colnames(random_dataset) = c(paste0("node_",1:(ncol(random_dataset)-1)),"Probs")
+	rownames(random_dataset) = paste0("sample_",1:nrow(random_dataset))
+	random_dataset[,"Probs"] = random_dataset[,"Probs"] / sum(random_dataset[,"Probs"])
+	
+	# set the names for the adjacency matrices
+	colnames(my_tree$structure) = paste0("node_",1:ncol(my_tree$structure))
+	rownames(my_tree$structure) = paste0("node_",1:ncol(my_tree$structure))
+	colnames(my_tree$probabilities) = paste0("node_",1:ncol(my_tree$probabilities))
+	rownames(my_tree$probabilities) = paste0("node_",1:ncol(my_tree$probabilities))
+	
+	# save the results
+	my_random_dataset = list(structure = my_tree$structure, probabilities = my_tree$probabilities, root_prob = my_tree$root_prob, dataset_samples = random_dataset)
+	
+	return(my_random_dataset)
+	
+}
+
+# generate a random tree (both structure and conditional probabilities)
+generate.random.tree <- function ( nodes, significance = 0.10 ) {
+	
+	# create the adjacency matrices to encode tree structure and probabilities
+	adj.matrix.structure = array(0,c(nodes,nodes))
+	adj.matrix.probabilities = array(0,c(nodes,nodes))
+	
+	# generate the tree structure randomly
+	my_tree = generate.random.tree.structure(nodes)
+	my_tree_structure = my_tree$structure
+	my_tree_root = my_tree$root
+	
+	# create the actual structure of the tree
+	root_prob = runif(1, min = significance, max = 1-significance)
+	for (i in 2:length(my_tree_structure)) {
+		curr_parents_nodes = my_tree_structure[[i-1]]
+		curr_children_nodes = my_tree_structure[[i]]
+		for (j in 1:length(curr_children_nodes)) {
+			curr_node = curr_children_nodes[j]
+			if(length(curr_parents_nodes)==1) {
+				curr_parent = curr_parents_nodes[1]
+			}
+			else {
+				curr_parent = sample(curr_parents_nodes,size=1)
+			}
+			curr_conditional_prob = runif(1, min = significance, max = 1-significance)
+			# set the values in the adjacency matrices
+			adj.matrix.structure[curr_parent,curr_node] = 1
+			adj.matrix.probabilities[curr_parent,curr_node] = curr_conditional_prob
+		}
+	}
+	
+	# get the leaves in the tree
+	leaves = NULL
+	for (i in 1:nrow(adj.matrix.structure)) {
+		nodes_children = which(adj.matrix.structure[i,]==1)
+		if(length(nodes_children)==0) {
+			leaves = c(leaves,i)
+		}
+	}
+	
+	# save the results
+	my_tree = list(structure = adj.matrix.structure, probabilities = adj.matrix.probabilities, root = my_tree_root, leaves = leaves, root_prob = root_prob)
+	
+	return(my_tree)
+	
+}
+
 # generate the structure of a random tree
 generate.random.tree.structure <- function ( nodes ) {
 	
@@ -163,6 +266,9 @@ generate.random.tree.structure <- function ( nodes ) {
 	else {
 		stop("I need at least 2 nodes in the tree!")
 	}
+	
+	# save the results
+	my_tree = list(structure = my_tree, root = my_tree[[1]])
 	
 	return(my_tree)
 	
