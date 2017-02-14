79d237b2eccbac1e4108370ef7af1b3044ee4f18
danro9685 <daniele.ramazzotti@yahoo.com>
2017-02-14 07:33:10 -0800

    saving for real data the best models to RData

--- experiment_real_data/single cell/dataset SCITE 3 - Breast - Single cell/find.score.R
+++ experiment_real_data/single cell/dataset SCITE 3 - Breast - Single cell/find.score.R
@@ -48,6 +48,8 @@ for(sample in 1:nrow(experiment.missing.data)) {
                 find.score$capri$id = exp
                 title = paste('\nCAPRI score:', round(find.score$capri$score, 3), ' id: ', find.score$capri$id)
                 plot_graph(experiment$reconstruction$capri$model, title, 'capri')
+                capri_best_model = experiment$reconstruction$capri$model
+                save(capri_best_model,file="capri_best_model.RData")
             } else if (experiment$reconstruction$capri$score == find.score$capri$score) {
                 find.score$capri$id = c(find.score$capri$id, exp)
             }
@@ -58,6 +60,8 @@ for(sample in 1:nrow(experiment.missing.data)) {
                 find.score$caprese$id = exp
                 title = paste('\nCAPRESE score:', round(find.score$caprese$score, 3), ' id: ', find.score$caprese$id)
                 plot_graph(experiment$reconstruction$capre$model, title, 'caprese')
+                caprese_best_model = experiment$reconstruction$caprese$model
+                save(caprese_best_model,file="caprese_best_model.RData")
             } else if (experiment$reconstruction$caprese$score == find.score$caprese$score) {
                 find.score$caprese$id = c(find.score$caprese$id, exp)
             }
@@ -68,6 +72,8 @@ for(sample in 1:nrow(experiment.missing.data)) {
                 find.score$prim$id = exp
                 title = paste('\nPRIM score:', round(find.score$prim$score, 3), ' id: ', find.score$prim$id)
                 plot_graph(experiment$reconstruction$prim$model, title, 'prim')
+                prim_best_model = experiment$reconstruction$prim$model
+                save(prim_best_model,file="prim_best_model.RData")
             } else if (experiment$reconstruction$prim$score == find.score$prim$score) {
                 find.score$prim$id = c(find.score$prim$id, exp)
             }
@@ -78,6 +84,8 @@ for(sample in 1:nrow(experiment.missing.data)) {
                 find.score$chowliu$id = exp
                 title = paste('\nCHOW LIU score:', round(find.score$chowliu$score, 3), ' id: ', find.score$chowliu$id)
                 plot_graph(experiment$reconstruction$chowliu$model, title, 'chowliu')
+                chowliu_best_model = experiment$reconstruction$chowliu$model
+                save(chowliu_best_model,file="chowliu_best_model.RData")
             } else if (experiment$reconstruction$chowliu$score == find.score$chowliu$score) {
                 find.score$chowliu$id = c(find.score$chowliu$id, exp)
             }
@@ -88,6 +96,8 @@ for(sample in 1:nrow(experiment.missing.data)) {
                 find.score$edmonds$id = exp
                 title = paste('\nEDMONDS score:', round(find.score$edmonds$score, 3), ' id: ', find.score$edmonds$id)
                 plot_graph(experiment$reconstruction$edmonds$model, title, 'edmonds')
+                edmonds_best_model = experiment$reconstruction$edmonds$model
+                save(edmonds_best_model,file="edmonds_best_model.RData")
             } else if (experiment$reconstruction$edmonds$score == find.score$edmonds$score) {
                 find.score$edmonds$id = c(find.score$edmonds$id, exp)
             }
@@ -98,6 +108,8 @@ for(sample in 1:nrow(experiment.missing.data)) {
                 find.score$gabow$id = exp
                 title = paste('\nGABOW score:', round(find.score$gabow$score, 3), ' id: ', find.score$gabow$id)
                 plot_graph(experiment$reconstruction$gabow$model, title, 'gabow')
+                gabow_best_model = experiment$reconstruction$gabow$model
+                save(gabow_best_model,file="gabow_best_model.RData")
             } else if (experiment$reconstruction$gabow$score == find.score$gabow$score) {
                 find.score$gabow$id = c(find.score$gabow$id, exp)
             }
