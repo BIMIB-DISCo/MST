c8c099593ba35da8b5855c6efb7e9b19a431fa44
luca-dex <luca.desano@gmail.com>
2017-02-22 16:39:02 +0100

    onconem navin

--- experiment_real_data/single_cell/breast_TNBC/generate.onconem.R
+++ experiment_real_data/single_cell/breast_TNBC/generate.onconem.R
@@ -9,13 +9,9 @@ if (! dir.exists('onconem')) {
 epos_level = 0.05
 eneg_level = 0.05
 
-load('data.RData')
-complete = data
-geno = as.genotypes(complete)
-geno = keysToNames(complete, geno)
-geno = geno[-6,]
-seed_level = round(runif(1) * 10000, 0)
-onconem.genotypes = t(geno)
+load('dataset_navin_TNBC.RData')
+data = dataset_navin_TNBC
+onconem.genotypes = t(data)
 
 oNEM = oncoNEM$new(Data = onconem.genotypes,
     FPR = epos_level,
@@ -54,51 +50,3 @@ plotTree(tree = oncoTree$g,
     axis = TRUE)
 
 dev.copy2pdf(file = 'onconem/onconem.best.clustered.pdf')
-
-
-load('data.merged.RData')
-ind = data.merged
-geno = as.genotypes(ind)
-geno = keysToNames(ind, geno)
-geno = geno[-6,]
-seed_level = round(runif(1) * 10000, 0)
-onconem.genotypes = t(geno)
-
-oNEM = oncoNEM$new(Data = onconem.genotypes,
-    FPR = epos_level,
-    FNR = eneg_level)
-
-oNEM$search(delta = 200)
-
-plotTree(tree = oNEM$best$tree,clones = NULL,vertex.size = 25)
-dev.copy2pdf(file = 'onconem/onconem.best.merged.pdf')
-
-oNEM.expanded = expandOncoNEM(oNEM,
-    epsilon = 10,
-    delta = 200,
-    checkMax = 10000,
-    app = TRUE)
-
-plotTree(tree = oNEM.expanded$best$tree,
-    clones = NULL,
-    vertex.size = 25)
-dev.copy2pdf(file = 'onconem/onconem.best.merged.expand.pdf')
-
-oncoTree = clusterOncoNEM(oNEM = oNEM.expanded, epsilon = 10)
-
-post = oncoNEMposteriors(tree = oncoTree$g,
-    clones = oncoTree$clones,
-    Data = oNEM$Data,
-    FPR = oNEM$FPR,
-    FNR = oNEM$FNR)
-
-edgeLengths = colSums(post$p_theta)[-1]
-
-plotTree(tree = oncoTree$g,
-    clones = oncoTree$clones,
-    e.length = edgeLengths,
-    label.length = 4,
-    axis = TRUE)
-
-dev.copy2pdf(file = 'onconem/onconem.best.merged.clustered.pdf')
-
